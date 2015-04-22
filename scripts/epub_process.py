4c3e89045d9aee4451d17ab590f92cd03b1e7ecb
gohai <gottfried.haider@gmail.com>
2015-04-22 15:03:11 +0200

    Replace broken merge by mattberos with the version from NN09

--- scripts/epub_process.py
+++ scripts/epub_process.py
@@ -5,11 +5,6 @@ from os.path import join
 from xml.etree import ElementTree as ET
 import html5lib
 import argparse
-<<<<<<< Updated upstream
-=======
-#from django.utils.html import urlize
-
->>>>>>> Stashed changes
 """
 (C) 2014 Andre Castro
 
@@ -42,18 +37,6 @@ fh.close()
 temp_dir="temp/"
 os.remove(temp_dir+'mimetype') # delete mimetype (will be added later with epub.writestr)
 
-<<<<<<< Updated upstream
-
-def rm_captioncode(tree, element):
-    for parag in tree.findall(element):        
-        caption = parag.findall('.//code[@class="caption"]')
-        if caption:
-            caption = caption[0]
-            parag.remove(caption)
-
-            
-=======
->>>>>>> Stashed changes
 def fn_rm_sup(tree, element): # Removes Footnotes <sub>
     for fn in tree.findall(element):
         for child in list(fn):
@@ -63,19 +46,11 @@ def fn_rm_sup(tree, element): # Removes Footnotes <sub>
                 fn.text=number
 
 def replace_fn_links(tree, element): #replace back arrows with work "back"
-<<<<<<< Updated upstream
     for parag in tree.findall(element):
         anchors = parag.findall("./a")
         for anchor in anchors:
             if '#fn'in anchor.get('href'):
                 anchor.text = 'back'
-=======
-    for tag in tree.findall(element):
-        if tag.text is not None:
-            text=(tag.text).encode('utf-8')
-            if text == '↩':#'&#8617;':
-                tag.text = 'back'
->>>>>>> Stashed changes
 
    
 def spine(filename): # makes cover & title page linear is <spine>
@@ -85,14 +60,8 @@ def spine(filename): # makes cover & title page linear is <spine>
     manifest = tree.find('.//{http://www.idpf.org/2007/opf}manifest')
     for child in spine.getchildren():
         if child.attrib['idref'] == 'cover_xhtml'or child.attrib['idref'] == 'title_page_xhtml':            
-<<<<<<< Updated upstream
-
-            #(child.attrib).pop("linear")
-            child.attrib['linear'] = 'yes'
-=======
             (child.attrib).pop("linear")
             #child.attrib['linear'] = 'yes'
->>>>>>> Stashed changes
     return tree
 
 
@@ -108,18 +77,6 @@ def save_html(content_dir, content_file, tree ):
 temp_ls=os.listdir("temp/")
 temp_ls.sort()
 
-<<<<<<< Updated upstream
-=======
-'''
-# URLIZE DEF - NEEDS DJANGO Installed
-url_regex = '.*http[s]?://.*' # any url                 
-def urlize_text(tree, element):
-    for tag in tree.findall(element):
-        if tag.text is not None:
-            text=(tag.text).encode('utf-8')
-            text=urlize(text, nofollow=False)
-'''
->>>>>>> Stashed changes
             
 for f in temp_ls: #loop epub contained files     
     if f[:2]=='ch' and f[-6:]==".xhtml": # all ch*.xhtml        
@@ -127,14 +84,7 @@ for f in temp_ls: #loop epub contained files
         xhtml = open(filename, "r") 
         xhtml_parsed = html5lib.parse(xhtml, namespaceHTMLElements=False)
         fn_rm_sup(xhtml_parsed, './/a[@class="footnoteRef"]')
-<<<<<<< Updated upstream
         replace_fn_links(xhtml_parsed, './/section[@class="footnotes"]/ol/li/p')        
-        rm_captioncode(xhtml_parsed, './/p')
-=======
-        replace_fn_links(xhtml_parsed, './/li/p/a')        
-#        urlize_text(xhtml_parsed, './/p')
-#        urlize_text(xhtml_parsed, './/li')
->>>>>>> Stashed changes
         
         save_html(
             content_dir=temp_dir,
@@ -166,10 +116,7 @@ for name in dirlist:
     path = name[5:] # removes 'temp/'
     epub.write(name, path, zipfile.ZIP_DEFLATED)
 
-<<<<<<< Updated upstream
     
-=======
->>>>>>> Stashed changes
 epub.close()
 
 # Step 4: clean up: rm temp zipname
